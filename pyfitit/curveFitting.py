Fix bugs in adaptive sampling and feff calculation

pyfitit/curveFitting.py
@@ -1,4 +1,4 @@
-import scipy, sklearn, copy, warnings
+import scipy, sklearn, copy, warnings, lmfit
 import numpy as np
 from lmfit.models import ExpressionModel, PolynomialModel
 from . import utils
@@ -93,6 +93,25 @@ def fit_to_experiment_by_norm_or_regression(exp_e, exp_xanes, fit_interval, fdmn
         else:
             assert normType == 'linearMult'
             return fdmnes_xan * (norm['a']*exp_e + norm['b']), norm
+
+
+def fit_by_polynom(e1, xan1, fit_interval):
+    e, xan = utils.limit(fit_interval, e1, xan1)
+
+    def model(t, e0, a):
+        ind = t < e0
+        res = np.zeros(t.shape)
+        res[~ind] = a*(t[~ind]-e0)**2
+        return 1 + res
+    mod = lmfit.Model(model, independent_vars=['t'])
+    params = mod.make_params(e0=5200, a=-1e-7)  # - стартовые
+    params['e0'].set(min=fit_interval[0], max=fit_interval[1])
+    params['a'].set(min=-1e-4, max=1e-4)
+    result = mod.fit(xan, params, t=e)
+    e0 = result.best_values['e0']
+    a = result.best_values['a']
+    app = model(e1,e0,a)
+    return app
 
 
 def findExpEfermi(exp_e, exp_xanes, search_shift_level):

pyfitit/feff.py
@@ -4,114 +4,125 @@ from io import StringIO
 import subprocess, os, tempfile, json
 from . import optimize, utils
 
-def distance(x, y, z):
-    return (x ** 2 + y ** 2 + z ** 2) ** 0.5
-# если строка folder пустая - создает внутри папки ./tmp и возвращает путь к созданной папке
-# energyRange - строка в формате FDMNES
 
-def generateInput(molecula, folder = '', temperature=None, debyeTemperature=None, **other):
+def generateInputUniqueAbsorber(molecule, folder, temperature, debyeTemperature, additional, absorber, edge, exchange, scf, rpath, exafs, **other):
+    os.makedirs(folder, exist_ok=True)
+    with open(folder + '/feff.inp', 'w') as f:
+        f.write
+        f.write(f'EDGE      {edge}\n')
+        f.write('S02       1.0\n')
+        f.write('CONTROL 1 1 1 1 1 1\n')
+        f.write('PRINT 1 0 0 0 0 0\n\n')
+        f.write(f'EXCHANGE  {exchange}\n')
+        f.write(f'SCF       {scf}\n\n')
+        f.write(f'DEBYE {temperature} {debyeTemperature}\n\n')
+        f.write(f'RPATH     {rpath}\n')
+        f.write(f'EXAFS     {exafs}\n')
+        if additional != '': f.write(additional + '\n')
+        if absorber is None: absorber = np.argmax(molecule.az)
+        assert 0 <= absorber <= len(molecule.az)-1, f'Wrong absorber index {absorber}'
+        f.write('POTENTIALS\n')
+        f.write('*ipot\tZ\ttag\n')
+        uniq_az, uniq_ind = np.unique(molecule.az, return_index=True)
+        ipot = np.zeros(len(molecule.az), dtype=int)
+        f.write(f'0\t{molecule.az[absorber]}\t{molecule.atomName[absorber]}\n')
+        absorberNamesakes = np.sum(molecule.az[absorber] == molecule.az) > 1
+        ip = 1
+        for ii in range(len(uniq_az)):
+            i = len(uniq_az)-1-ii
+            if (not absorberNamesakes) and molecule.az[absorber] == uniq_az[i]: continue
+            ind = uniq_ind[i]
+            f.write(f'{ip}\t{molecule.az[ind]}\t{molecule.atomName[ind]}\n')
+            ipot[molecule.az == molecule.az[ind]] = ip
+            ip += 1
+        ipot[absorber] = 0
+        f.write('\nATOMS\n*x\ty\tz\tipot\ttag\tdistance\n')
+        for i in range(len(molecule.az)):
+            x,y,z = molecule.atom[i]
+            d = np.linalg.norm(molecule.atom[i]-molecule.atom[absorber])
+            f.write(f'{x}\t{y}\t{z}\t{ipot[i]}\t{molecule.atomName[i]}\t{d}\n')
+        f.write('\nEND\n')
+
+
+def generateInput(molecule, folder='', temperature=0, debyeTemperature=0, additional='', absorbers=None, edge='K', exchange=0, scf=5, rpath=5, exafs=20, **other):
+    """
+    Generate input for feff8.5 calculation
+
+    :param molecule:
+    :param folder:
+    :param temperature:
+    :param debyeTemperature:
+    :param additional:
+    :param absorbers: one index (from 0) of absorber atom or list of indexes. Default: take heaviest atom
+    """
+
     if 'Debye Temperature' in other:
         debyeTemperature = other['Debye Temperature']
     if folder == '':
         if not os.path.exists("./tmp"): os.makedirs("./tmp")
         folder = tempfile.mkdtemp(dir='./tmp')
+    os.makedirs(folder, exist_ok=True)
+    if isinstance(absorbers, list):
+        with open(folder+os.sep+'absorbers.txt', 'w') as f: f.write(' '.join(map(str, absorbers)))
+        for absorber in absorbers:
+            generateInputUniqueAbsorber(molecule, folder+os.sep+f'absorber_{absorber}', temperature, debyeTemperature, additional, absorber, edge, exchange, scf, rpath, exafs, **other)
     else:
-        os.makedirs(folder, exist_ok=True)
-
-    with open(folder + '/feff.inp', 'w') as f:
-        f.write
-        f.write('EDGE      K\n')
-        f.write('S02       1.0\n')
-        f.write('CONTROL 1 1 1 1 1 1\n')
-        f.write('PRINT 1 0 0 0 0 3\n\n')
-        f.write('EXCHANGE  0\n')
-        f.write('SCF       5.0\n\n')
-        f.write('DEBYE ' + str(temperature) + ' ' + str(debyeTemperature) + '\n\n')
-        f.write('RPATH     5.0\n')
-        f.write('EXAFS     20\n')
-        #center = molecula.mol.loc[0,['x','y','z']].values
-        ls = []
-        d = dict()
-        for i in range(molecula.atom.shape[0]):
-            a = molecula.atom[i, :]
-            # pn - proton number
-            x, y, z, pn, atom_name = a[0], a[1], a[2], molecula.atomNumber[i], molecula.atomName[i]
-            if atom_name not in d:
-                d[atom_name] = pn
-            ls.append((x, y, z, pn, atom_name, distance(x, y, z)))
-        f.write('POTENTIALS\n') #TODO
-        f.write('*ipot\tZ\ttag\n')
-        sorted_d = sorted(d.items(), key=lambda kv: (kv[1], kv[0]), reverse=True)
-        if len(sorted_d) > 0:
-            f.write('0\t{0}\t{1}\n'.format(sorted_d[0][1], sorted_d[0][0]))
-        else:
-            f.write('NONE\n')
-        equal_mol_count = np.sum(molecula.atomNumber == sorted_d[0][1])
-        if equal_mol_count > 1:
-            j = -1
-            flag = False
-        else:
-            j = 0
-            flag = True
-        for tag, Z in sorted_d:
-            if flag:
-                flag = False
-                continue
-            j += 1
-            f.write('{0}\t{1}\t{2}\n'.format(j, Z, tag))
-        j = 0
-        dict_ipot = dict()
-        for tag, _ in sorted_d:
-            dict_ipot[tag] = j
-            j += 1
-        f.write('\nATOMS\n*x\ty\tz\tipot\ttag\tdistance\n')
-        ls.sort(key=lambda x: x[5])
-        for x in ls:
-            #f.write('{0[0]: f}\t{1[1]: f}\t{2[2]: f}\t{3[3]: 3}\t{4[4]}\t{5[5]:f}\n'.format(x))
-            f.write('{x: f}\t{y: f}\t{z: f}\t{ipot: 3}\t{tag}\t{distance:f}\n'.format(x=x[0], y=x[1], z=x[2], ipot=dict_ipot[x[4]], tag=x[4]+'1.1', distance=x[5]))
-        '''
-        for i in range(molecula.mol.shape[0]):
-            a = molecula.mol.loc[i, :]
-            x, y , z = a['x'], a['y'], a['z']
-            f.write('{0: f}\t{1: f}\t{2: f}\t{3: 3}\t{4}\t{5}\n'.format(x, y, z, a['proton_number'], a['atom_name'] + '1.0', distance(x, y, z)))
-        '''
-        f.write('\nEND\n')
-
-    #with open(folder + '/feff.txt', 'w') as f:
-        #f.write('1\nin.txt')
+        generateInputUniqueAbsorber(molecule, folder, temperature, debyeTemperature, additional, absorbers, edge, exchange, scf, rpath, exafs, **other)
     return folder
 
-def runLocal(folder = '.'):
+
+def getAbsorberFolders(folder, returnAbsorberNum=False):
+    if not os.path.exists(folder+os.sep+'absorbers.txt'):
+        if returnAbsorberNum: return [folder], [0]
+        else: return [folder]
+    with open(folder+os.sep+'absorbers.txt') as f: ab = f.read().strip().split(' ')
+    fs = [folder+os.sep+f'absorber_{a}' for a in ab]
+    if returnAbsorberNum: return fs, list(map(int,ab))
+    else: return fs
+
+
+def runLocal(folder):
     feff = 'feff85L.exe' if os.name == 'nt' else 'feff85L'
-    proc = subprocess.Popen([feff, 'feff.inp'], cwd=folder, stdout=subprocess.PIPE)
-    proc.wait()
-    if proc.returncode != 0:
-      raise Exception('Error while executing "'+feff+'" command')
-    return proc.stdout.read()
+    output = ''
+    for f in getAbsorberFolders(folder):
+        proc = subprocess.Popen([feff, 'feff.inp'], cwd=f, stdout=subprocess.PIPE)
+        stdoutdata, stderrdata = proc.communicate()
+        if proc.returncode != 0:
+            raise Exception('Error while executing "' + feff + '" command:\n' + str(stderrdata))
+        output += str(stdoutdata) + '\n\n'
+    return output
 
-def runCluster(folder = '.', memory=5000, nProcs = 1):
-    proc = subprocess.Popen(["run-cluster-and-wait", "-m", str(memory), '-n', str(nProcs), "feff85L feff.inp"], cwd=folder, stdout=subprocess.DEVNULL, stderr=subprocess.DEVNULL)
-    proc.wait()
 
-def calc_header_size(fileName):
-    with open(fileName, 'r') as f:
-        lines = f.read().split('\n')
-    matches = [i for i in range(len(lines)) if lines[i].find('#       k          chi') >= 0] # '      k          chi'
-    assert len(matches) == 1, "wrong feff output file format"
-    return matches[0] + 1
-
+def runCluster(folder, memory=5000, nProcs=1):
+    for f in getAbsorberFolders(folder):
+        proc = subprocess.Popen(["run-cluster-and-wait", "-m", str(memory), '-n', str(nProcs), "feff85L feff.inp"], cwd=f, stdout=subprocess.DEVNULL, stderr=subprocess.DEVNULL)
+        proc.wait()
 
 
 def getParams(fileName):
     with open(fileName, 'r') as f: params0 = json.load(f)
     return [p[0] for p in params0], [p[1] for p in params0]
 
-def parse_one_folder(d):
-    exafsFile = d+os.sep+'chi.dat'
-    if not os.path.isfile(exafsFile):
-        raise Exception('Error: in folder ' + d + ' there is no output file with energies')
-    skip_header = calc_header_size(exafsFile)
-    exafs = pd.read_csv(exafsFile, skiprows = skip_header, header=None, sep='\s+')
-    energies = exafs.loc[:,0].ravel()
-    exafs = exafs.loc[:,1].ravel()
-    return utils.Exafs(energies, exafs)
+
+def parse_one_folder(folder, multipleAbsorber=False):
+    results = {}
+    ds, absorbers = getAbsorberFolders(folder, returnAbsorberNum=True)
+    for i, d in enumerate(ds):
+        exafsFile = d+os.sep+'chi.dat'
+        if not os.path.isfile(exafsFile):
+            raise Exception('Error: in folder ' + d + ' there is no output file chi.dat')
+        s = utils.readSpectrum(exafsFile, guess=True)
+        results[absorbers[i]] = utils.Exafs(s.energy, s.intensity)
+
+    # make all spectra have the same point count
+    enNums = [len(results[a].k) for a in absorbers]
+    maxEnInd = np.argmax(enNums)
+    maxEnNum = enNums[maxEnInd]
+    me = results[absorbers[maxEnInd]]
+    for a in absorbers:
+        if len(results[a].k) != maxEnNum:
+            results[a] = utils.Exafs(me.k, np.interp(me.k, results[a].k, results[a].chi))
+    if multipleAbsorber: return results
+    else:
+        assert len(results) == 1, 'Multiple absorbers detected. Set multipleAbsorber=True'
+        return list(results.values())[0]